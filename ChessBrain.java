56944dd8db24b92978d7586398ccdc245b0e1b88
vsuperdev <rentaccount@yahoo.com>
2014-09-17 21:52:34 -0400

    This is commit to merge back pawn code. version 3.

--- ChessBrain.java
+++ ChessBrain.java
@@ -8,29 +8,29 @@ public class ChessBrain
 		//piecesClass properties
 				//pieces move
 				//possible moves of the selected piece should be highlighted
-					//future cell can not be occupied with the same color piece
+					//future cellPosition can not be occupied with the same color piece
 					//after the move, if the kind of opposite color is attached, send the check message
 				//pieces capture or get captured
 					// no piece can capture another piece of the same color
-					// two pieces can not share one cell
+					// two pieces can not share one cellPosition
 					// residing piece is considered captured when other piece moves in to it space
 					//when piece is captured it moves out of the board
 					// no pieces can occupy a space outside of the board unless captured
 				//pieces can not jump other pieces to move in to an existing space regardless of the color, except for the knight
-				//pieces can occupy any colored cell except for bishops
-				//pieces have to be inside a cell, can not occupy a space in between the cells
+				//pieces can occupy any colored cellPosition except for bishops
+				//pieces have to be inside a cellPosition, can not occupy a space in between the cellPositions
 				//for pawn, king and rook first move needs to set a flag
 				
 				//!!!!!!!!!!!!!!!!!!!!!idea!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 				// rooks movement is same as extended pawn horizontal and vertical
-				// bishop's movement is same as pawn capturing piece from cross cell
+				// bishop's movement is same as pawn capturing piece from cross cellPosition
 				// knights movement is equivalent to one pawn move and pawn capture with or without capture
 				//queen moves as a combination of rook and bishop
 				// king moves same as queen but is limited to only one adjacent step
 				
 				//!!!!!!!!!!!!!!!!!!!!!observation!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 				//only 3 types of capture exist
-				// 1. pawn capturing in cross cell
+				// 1. pawn capturing in cross cellPosition
 				// 2. different colored pieces blocking movement of current piece being captured
 				// 3. passing pawn capture
 	}
@@ -72,10 +72,10 @@ public class ChessBrain
 			position[1] = p[1];
 		}
 		
-// 		this will get the appropriate color of cell. 		
-//		cellAdd = row+column;
+// 		this will get the appropriate color of cellPosition. 		
+//		cellPositionAdd = row+column;
 //		
-//		if ( (cellAdd mod 2) == 0)
+//		if ( (cellPositionAdd mod 2) == 0)
 //
 //		{
 //		    isEven = true
@@ -94,45 +94,76 @@ public class ChessBrain
 		//rules for Pawn2
 		//select of an appropriate color for the current
 		//pawn position is known by selection of the pawn
-		int[][] currentPosition = cell[row][column];
+		int[] currentPosition = cellPosition[2];
 
 //		pawn can be white or black, pawn color is obtained from brain class
 
 //		Pawn can only move one step forward
 //		except for the first time move, where it can choose to move two steps
 		
-		int i=1; //keep track of number of cells to be highlighted
-		int[][] highlight; 
-		int[][] highlightCell = 0;//keeps track of number of cells to be highlighted
+		int i=0; //keep track of number of cellPositions to be highlighted
+		int row=cellPosition[0];
+		int clm=cellPosition[1]; 
+		int[][2] possibleMove;
+		//****need a highlight method to take row and column values and highlight the rows
 		
 			//if the color is white and row is 2, pawn is at initial position
 			if (moveColor==true || row == 2) {	
-				highlight[i][i] = cell[row+1][column]; 
+				highlight (row+1,clm); 
+				possibleMove[i][0]=row+1;
+				possibleMove[i][1]=clm;
 				i++;
-				highlight[i][i] = cell[row+2][column];
+				highlight (row+2,clm); 
+				possibleMove[i][0]=row+2;
+				possibleMove[i][1]=clm;
 				i++;
 	
 			}
 			//if the color is black and row is 7, pawn is at initial position
 			else if (moveColor==false || row == 7){ 
-				highlight[i][i] = cell[row+1][column];
+				highlight (row+1,clm); 
+				possibleMove[i][0]=row+1;
+				possibleMove[i][1]=clm;
 				i++;
-				highlight[i][i] = cell[row+2][column];
+				highlight (row+2,clm); 
+				possibleMove[i][0]=row+2;
+				possibleMove[i][1]=clm;
 				i++;
-				
 			}
 			
 			//if pawn is not at initial position can not move more then one step
 			else{
-				highlight[i][i] = cell[row+1][column]; 
+				highlight (row+1,clm); 
+				possibleMove[i][0]=row+1;
+				possibleMove[i][1]=clm;
 				i++;
 			}
 			
-
-//		pawn can only capture one step cross to its cell except for 
+//		pawn can only capture one step cross to its cellPosition except for 
+			//****need a cellPosition method to take row and column values return boolean value false for empty, true for piece
+			if ((cellPositionEmpty(row,column))!=true){//method to find out if the cellPosition is not Empty
+				if (moveColor!=pieceColor(row+1,clm+1)) {	//****need a pieceColor method to take row and column values return boolean value false for same color as turn, true for opposite color
+					highlight (row+1,clm+1); 
+					possibleMove[i][0]=row+1;
+					possibleMove[i][1]=clm+1;
+					i++;
+				}
+				else if (moveColor!=pieceColor(row+1,clm-1)){
+					highlight (row+1,clm-1); 
+					possibleMove[i][0]=row+1;
+					possibleMove[i][1]=clm-1;
+					i++;
+				}
+				else{//just for practice
+					
+				}
+			}
+			else{//just for practice
+				
+			}
 //		option to create any other piece when pawn reaches next side
 //		pawn can not move backwards
-//			en passant Pawn capture when passing
+//		en passant Pawn capture when passing
 		
 
 	}
